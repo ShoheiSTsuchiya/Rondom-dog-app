e9727a63a238eb78fd48b27f71612fbde43bdb08
shohei <shohei.t.0920@gmail.com>
2023-11-12 18:00:14 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+function DogPictureComponent() {
+  const [dogImageUrl, setDogImageUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Using Axios to fetch data from the dog API
+    axios.get('https://dog.ceo/api/breeds/image/random')
+      .then(response => {
+        setDogImageUrl(response.data.message); // Extracting the image URL
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  // Function to render the dog image
+  function renderDogImage() {
+    if (dogImageUrl) {
+      return <img src={dogImageUrl} alt="Random Dog" />;
+    }
+    return null; // Return null if no image URL is present
+  }
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Dog Picture</h1>
+      {renderDogImage()} {/* Call the function to render the dog image */}
     </div>
   );
 }
 
-export default App;
+export default DogPictureComponent;
